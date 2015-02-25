bfd8d86eef29b34bee0605ddc336ca2222d9ebd7
Alexey ALexandrov <aleksandrov1988@gmail.com>
2015-02-25 19:53:33 +0300

    Small fixes

--- files/app/helpers/copyright_helper.rb
+++ files/app/helpers/copyright_helper.rb
@@ -2,8 +2,8 @@ module CopyrightHelper
   def copyright_years
     year1=2013
     year2=Date.today.year
-    res="© t('app.name'), "
-    res+=year1==year2 ? "\#{year1}" : "\#{year1}–\#{year2}"
+    res="© #{t('app.name')}, "
+    res+=year1==year2 ? "#{year1}" : "#{year1}–#{year2}"
     content_tag(:span, res)
   end
 end
